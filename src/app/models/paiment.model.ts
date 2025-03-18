921b7d7bae57706c1e62970ac1721c835f6267f7
nezlioui <idirnezlioui4@gmail.com>
2025-03-18 11:01:52 +0100

    paiment en cours

---
+++ src/app/models/paiment.model.ts
@@ -0,0 +1,11 @@
+export interface paiements {
+    id_paiement:number
+    montant_paye:number
+    date_paiement: Date
+    date_max_paiement:Date
+    solde_restant:number
+    statut_paiment:string
+    remise:number
+    id_etudiant:number
+}
+ 
--- src/app/pages/student-paiment/student-paiment.component.ts
+++ src/app/pages/student-paiment/student-paiment.component.ts
@@ -6,44 +6,11 @@ import { NavbarComponent } from "../../components/navbar/navbar.component";
 
 @Component({
   selector: 'app-student-paiment',
-  imports: [FormsModule, CommonModule, NavbarComponent],
+  imports: [FormsModule, CommonModule],
   templateUrl: './student-paiment.component.html',
   styleUrl: './student-paiment.component.css'
 })
 export class StudentPaimentComponent {
-  @Input() etudiant!: Etudiant;
-  @Output () paimentEffectue=new EventEmitter<boolean>()
-
-
-  montant:number=0
-  paimentValide=false
-  
-  ngOnChanges() {
-    if (this.etudiant) {
-      console.log('Étudiant dans le composant de paiement:', this.etudiant);
-    } else {
-      console.log('Aucun étudiant sélectionné');
-    }
-  }
-
-  effectuerPaiment(){
-    if (this.montant>0) {
-      this.paimentValide=true
-      this.paimentEffectue.emit(true)
-      alert(`paiment de ${this.montant} € pour ${this.etudiant.nom} effectuer`)
-    }else{
-      alert("le montant doit etre superieure")
-    }
-  }
-  imprimerRecu(){
-    alert(`Reçu de paiment imprimer pour l'etudiant ${this.etudiant.nom} ${this.etudiant.prenom}`)
-  }
-  imprimerAttestation(){
-    alert(`Attestation d'insecription imprimé ${this.etudiant.nom} ${this.etudiant.prenom}`)
-  }
-
-  imprimeCertificat(){
-    alert(`certificat de scolarite imprimeé ${this.etudiant.nom} ${this.etudiant.prenom}`)
-  }
+ 
 
 }
